2f550b6f765ad06daadb49bdbb3b56ac966df421
Joel <rootjdev@gmail.com>
2023-04-17 16:04:13 +0930

    subdir for each host

--- bench.sh
+++ bench.sh
@@ -5,14 +5,14 @@ set -e # die on error
 # base is the upsteam project
 #
 clean() {
-  rm -rf asm c c52 fiat_c fiat_cryptopt
+  rm -rf "$(hostname)"{asm,c,c52,fiat_c,fiat_cryptopt}
   rm -- *.log
 }
 
 default_asm() {
   # we compile to use the hand written asembly
 
-  dir=asm
+  dir=$(hostname)asm
   cp -r ./base "${dir}"
   pushd "${dir}"
   ./autogen.sh
@@ -21,13 +21,13 @@ default_asm() {
 
   popd
 
-  ./${dir}/bench_internal >./${dir}_bench_internal.log
-  ./${dir}/bench_ecmult >./${dir}_bench_ecmult.log
+  "./${dir}/bench_internal" >"./${dir}_bench_internal.log"
+  "./${dir}/bench_ecmult" >"./${dir}_bench_ecmult.log"
 }
 
 default_c() {
   # we compile to use the upsteam C version which contains 810
-  dir=c
+  dir=$(hostname)c
   cp -r ./base "${dir}"
   pushd "${dir}"
   ./autogen.sh
@@ -36,13 +36,13 @@ default_c() {
 
   popd
 
-  ./${dir}/bench_internal >./${dir}_bench_internal.log
-  ./${dir}/bench_ecmult >./${dir}_bench_ecmult.log
+  "./${dir}/bench_internal" >"./${dir}_bench_internal.log"
+  "./${dir}/bench_ecmult" >"./${dir}_bench_ecmult.log"
 }
 
 default_c52() {
   # we compile to use the C version which does not contain 810 by reverting the patch
-  dir=c52
+  dir=$(hostname)c52
   cp -r ./base "${dir}"
   cp ./field_5x52_asm_impl_before_810.h "${dir}"/src/field_5x52_int128_impl.h
   pushd "${dir}"
@@ -53,14 +53,13 @@ default_c52() {
 
   popd
 
-  ./${dir}/bench_internal >./${dir}_bench_internal.log
-  ./${dir}/bench_ecmult >./${dir}_bench_ecmult.log
-
+  "./${dir}/bench_internal" >"./${dir}_bench_internal.log"
+  "./${dir}/bench_ecmult" >"./${dir}_bench_ecmult.log"
 }
 
 fiat_c() {
   # we replace the C versions.
-  dir=fiat_c
+  dir=$(hostname)fiat_c
   cp -r ./base "${dir}"
   cp ./secp256k1_dettman_64.c "${dir}"/src
 
@@ -94,14 +93,13 @@ EOF
 
   popd
 
-  ./${dir}/bench_internal >./${dir}_bench_internal.log
-  ./${dir}/bench_ecmult >./${dir}_bench_ecmult.log
-
+  "./${dir}/bench_internal" >"./${dir}_bench_internal.log"
+  "./${dir}/bench_ecmult" >"./${dir}_bench_ecmult.log"
 }
 
 fiat_cryptopt() {
   # we replace the C versions.
-  dir=fiat_cryptopt
+  dir=$(hostname)fiat_cryptopt
   cp -r ./base "${dir}"
   cp ./field_5x52_asm_impl_cryptopt.h ./field_5x52_asm_impl_cryptopt.c "${dir}"/src
   pushd "${dir}"
@@ -119,9 +117,8 @@ fiat_cryptopt() {
 
   popd
 
-  ./${dir}/bench_internal >./${dir}_bench_internal.log
-  ./${dir}/bench_ecmult >./${dir}_bench_ecmult.log
-
+  "./${dir}/bench_internal" >"./${dir}_bench_internal.log"
+  "./${dir}/bench_ecmult" >"./${dir}_bench_ecmult.log"
 }
 
 clean
